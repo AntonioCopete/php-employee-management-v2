ecf501591a384147295ac38a7bb7b0ae91cd4fb2
tamtran <tamtran2885@gmail.com>
2021-11-26 13:18:48 +0100

    add employee data

--- resources/db.sql
+++ resources/db.sql
@@ -1,9 +1,31 @@
-
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-11-2021 a las 13:17:36
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `employee_v2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `address`
+--
 
 CREATE TABLE `address` (
   `id` int(255) NOT NULL,
@@ -13,6 +35,12 @@ CREATE TABLE `address` (
   `postalCode` varchar(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `employee`
+--
+
 CREATE TABLE `employee` (
   `id` int(255) NOT NULL,
   `name` varchar(40) NOT NULL,
@@ -20,30 +48,68 @@ CREATE TABLE `employee` (
   `email` varchar(40) NOT NULL,
   `gender` enum('Male','Female') NOT NULL,
   `age` int(2) NOT NULL,
-  `phoneNumber` int(15) NOT NULL,
+  `phoneNumber` varchar(40) NOT NULL,
   `adressId` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `employee`
+--
 
+INSERT INTO `employee` (`id`, `name`, `lastName`, `email`, `gender`, `age`, `phoneNumber`, `adressId`) VALUES
+(1, 'Rack', 'Lei', 'jackon@network.com', 'Male', 24, '2147483647', 1),
+(2, 'John', 'Doe', 'jhondoe@foo.com', 'Male', 34, '1283645645', 2),
+(3, 'Leila', 'Mills', 'mills@leila.com', 'Female', 29, '2147483647', 3),
+(4, 'Richard', 'Desmond', 'dismond@foo.com', 'Male', 30, '90876987654', 4),
+(5, 'Susan', 'Smith', 'susanmith@baz.com', 'Female', 28, '224355488976', 5),
+(6, 'Brad', 'Simpson', 'brad@foo.com', 'Male', 40, '6854634522', 6),
+(7, 'Neil', 'Walker', 'walkerneil@baz.com', 'Male', 42, '4537278819', 7),
+(8, 'Robert', 'Thomson', 'jackon@network.com', 'Male', 24, '91232876454', 8);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `address`
+--
 ALTER TABLE `address`
   ADD PRIMARY KEY (`id`);
 
-
+--
+-- Indices de la tabla `employee`
+--
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`),
   ADD KEY `adressId` (`adressId`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `address`
+--
 ALTER TABLE `address`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
-
+--
+-- AUTO_INCREMENT de la tabla `employee`
+--
 ALTER TABLE `employee`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `address`
+--
 ALTER TABLE `address`
   ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`id`) REFERENCES `employee` (`adressId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
